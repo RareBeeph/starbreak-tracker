390b2c3e869a0d9f3da87d98e192dee6415b5eb2
Aria Taylor <ari@aricodes.net>
2023-01-09 18:20:55 -0500

    Add foreign key to drop table

--- backend/backend/db/models/drop.py
+++ backend/backend/db/models/drop.py
@@ -1,7 +1,9 @@
 from .base import BaseModel
-from peewee import BigBitField, BooleanField
+from .item import Item
+from peewee import BigBitField, BooleanField, ForeignKeyField
 
 
 class Drop(BaseModel):
+    item = ForeignKeyField(Item, backref="drops")
     modifiers = BigBitField()  # Include repeats separate from unrepeated forms
     is_elite_boss_drop = BooleanField()  # Distinguishes 0-3 mod and 1-4 mod samples
