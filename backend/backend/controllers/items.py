10c97517fc1eb0a27302810530ea8f240c410199
Keiran Jensen <keiranjensen@gmail.com>
2023-01-12 20:18:49 -0500

    Implement GET, POST, PATCH, DELETE routes for Items

--- backend/backend/controllers/items.py
+++ backend/backend/controllers/items.py
@@ -1,4 +1,7 @@
 from flask import Blueprint
+from flask import request
+
+import json
 
 from backend.db.models import Item
 from backend.serializers import ItemSchema
@@ -9,3 +12,25 @@ bp = Blueprint("items", __name__, url_prefix="/items")
 @bp.route("/", methods=("GET",))
 def index():
     return ItemSchema().dump(Item.select().order_by(Item.name), many=True)
+
+
+@bp.route("/<int:item_id>", methods=("GET",))
+def get_item_by_id(item_id):
+    return ItemSchema().dump(Item.get_by_id(item_id))
+
+
+@bp.route("/", methods=("POST",))
+def post_item():
+    return [ItemSchema().load(request.get_json()).save()]
+
+
+@bp.route("/<int:item_id>", methods=("POST", "PUT", "PATCH"))
+def patch_item(item_id):
+    requested_item = ItemSchema().load(request.get_json())
+    requested_item.id = item_id
+    return [requested_item.save()]
+
+
+@bp.route("/<int:item_id>", methods=("DELETE",))
+def delete_item(item_id):
+    return [Item.get_by_id(item_id).delete_instance()]
--- backend/backend/serializers/item.py
+++ backend/backend/serializers/item.py
@@ -1,4 +1,7 @@
 from marshmallow import fields
+from marshmallow import post_load
+
+from backend.db.models.item import Item
 from .base import BaseSchema
 
 
@@ -6,3 +9,7 @@ class ItemSchema(BaseSchema):
     equip_slot = fields.Str()
     name = fields.Str()
     supported_modifiers = fields.Integer()
+
+    @post_load
+    def make_item(self, data, **kwargs):
+        return Item(**data)
