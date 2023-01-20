2930c74903d66ed7a362a8374917590b399b9564
Keiran Jensen <keiranjensen@gmail.com>
2023-01-19 20:47:24 -0500

    Add more item table tests and a setup fixture

--- backend/tests/controllers/test_items.py
+++ backend/tests/controllers/test_items.py
@@ -1,6 +1,5 @@
 from backend.db.models import Item
 from backend.serializers import ItemSchema
-from backend.db import conn
 from faker import Faker
 from peewee import SqliteDatabase
 import pytest
@@ -13,18 +12,15 @@ fake = Faker()
 FILL_QUANTITY = 3
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def clean_fill_items(test_client) -> None:
-    conn = SqliteDatabase("file::memory:")  # might interfere with later tests
-    conn.create_tables(Item)
+    conn = SqliteDatabase("file::memory:")
+    Item._meta.database = conn
+    conn.create_tables([Item])
+
     for i in range(FILL_QUANTITY):  # loop runs every time the fixture is accessed
-        test_client.post(
-            f"{resource}/",
-            json=schema.dump(
-                Item(name=fake.name(), equip_slot=fake.ean(length=13)),
-            ),
-        )  # relies on the validity of posting via the client. avoid circular logic
-    return test_client.get(f"{resource}/")  # relies on the validity of getting all rows
+        Item(name=fake.name(), equip_slot=fake.ean(length=13)).save(),
+    assert Item.select().count() == FILL_QUANTITY
 
 
 def test_root(test_client) -> None:
@@ -42,6 +38,29 @@ def test_create(test_client) -> None:
     assert response.status_code == 200
 
 
-def test_get_by_id(test_client, clean_fill_items) -> None:
-    response = test_client.get(f"{resource}/")  # does not yet query by id
+def test_get_by_id(test_client) -> None:
+    i = Item.get().id
+    response = test_client.get(f"{resource}/{str(i)}")
     assert response.status_code == 200
+
+
+def test_patch(test_client) -> None:
+    i = Item.get().id
+    patch = Item(name=fake.name(), equip_slot=fake.ean(length=13))
+
+    response = test_client.post(
+        f"{resource}/{str(i)}",
+        json=schema.dump(patch),
+    )
+    assert response.status_code == 200
+
+    patch.id = i
+    assert Item.get_by_id(i) == patch
+
+
+def test_delete(test_client) -> None:
+    i = Item.get().id
+
+    response = test_client.delete(f"{resource}/{str(i)}")
+    assert response.status_code == 200
+    assert Item.select().where(Item.id == i).count() == 0
